c0656a711dd85fc4e1334600cca4f7cc5fccd132
Mohamed_Hesham <mohamedhesham0097@gmail.com>
2023-11-13 00:43:02 +0200

    Still ongoing...

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const CreateUser = () => {
   return (
-    <div>CreateUser</div>
+    <div className='d-felx vh-100 bg-primary justify-content-center align-items-center' >
+      <div className='w-50 bg-white rounded p-3' >
+          <form>
+            <h2>Add User</h2>
+            <div className='mb-2' >
+                <label htmlFor='' >Name</label>
+                <input type='text' placeholder='Enter Your Name' className='form-control' ></input>
+            </div>
+            <div className='mb-2' >
+            <input type='email' placeholder='Enter Your E-Mail' className='form-control' ></input>
+            </div>
+            <div className='mb-2' >
+            <input type='Age' placeholder='Enter Your Age' className='form-control' ></input>
+            </div>
+            <button className='btn btn-success' >Submit</button>
+          </form>
+      </div>
+    </div>
   )
 }
 
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Users = () => {
     const [users, setUsers]= useState([{
@@ -9,6 +10,7 @@ const Users = () => {
   return (
     <div className=" d-flex vh-100 bg-primary justify-content-center align-items-center " >
         <div className=' w-50 bg-white rounded p-3 ' >
+            <Link to="/create" className='btn btn-success' >Add +</Link>
             <table className='table' >
                 <thead>
                     <tr>
@@ -21,11 +23,14 @@ const Users = () => {
                 <tbody>
                     {
                         users.map((user)=> {
-                            <tr>
-                                <td>{users.Name}</td>
-                                <td>{users.Email}</td>
-                                <td>{users.Age}</td>
-                                <td> <button>Edit</button> <button>Delete</button> </td>
+                            return  <tr>
+                                <td>{user.Name}</td>
+                                <td>{user.Email}</td>
+                                <td>{user.Age}</td>
+                                <td> 
+                                    <Link to="/update" className='btn btn-success' >Update</Link>
+                                    <button>Delete</button> 
+                                </td>
                             </tr>
                         } )
                     }
