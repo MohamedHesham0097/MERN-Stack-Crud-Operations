955257d6e66edf989bfb5a502606585da77ecf9b
Mohamed_Hesham <mohamedhesham0097@gmail.com>
2023-11-14 19:01:15 +0200

    In Progress...

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const CreateUser = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [age, setAge] = useState()
+  const Submit = (e) => {
+    e.preventDefault()
+    axios.post("http://localhost:3001/createUser", {name,email,age})
+    .then(result => console.log(result))
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className='d-felx vh-100 bg-primary justify-content-center align-items-center' >
       <div className='w-50 bg-white rounded p-3' >
-          <form>
+          <form onSubmit={Submit}>
             <h2>Add User</h2>
             <div className='mb-2' >
                 <label htmlFor='' >Name</label>
-                <input type='text' placeholder='Enter Your Name' className='form-control' ></input>
+                <input type='text' placeholder='Enter Your Name' className='form-control' 
+                onChange={(e) => setName(e.target.value)} />
             </div>
             <div className='mb-2' >
-            <input type='email' placeholder='Enter Your E-Mail' className='form-control' ></input>
+            <input type='email' placeholder='Enter Your E-Mail' className='form-control' 
+            onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className='mb-2' >
-            <input type='Age' placeholder='Enter Your Age' className='form-control' ></input>
+            <input type='Age' placeholder='Enter Your Age' className='form-control' 
+            onChange={(e) => setAge(e.target.value)} />
             </div>
             <button className='btn btn-success' >Submit</button>
           </form>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const UserModel = require('./models/Users')
+// const { default: Users } = require('../client/src/Users')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+app.post("/createUser",(req, res)=>{
+    UserModel.create(req.body)
+    .then(Users => res.json(Users))
+    .catch(err => res.json(err))
+})
 
 app.listen(3001, ()=>{
     console.log('Server listening on')
